13eb87944c8e6cd9cf9066773bb6a2797d0f61e0
Aiden Benoualid <aidenbenoualid@Aidens-MacBook-Air-2.local>
2026-05-10 22:52:52 -0400

    load leaflet css via cdn link

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,14 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+          integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+          crossOrigin=""
+        />
+      </head>
       <body className={`${inter.className} bg-cream text-brown-dark antialiased`}>
         {children}
       </body>
